5e70d00acd5b6fa149024d3c963c83520061f47e
Stephen-BravoLabs <stephenblevins@bravolabs.xyz>
2025-05-07 14:42:49 +1100

    added liscense comments

--- contracts/DataStructs.sol
+++ contracts/DataStructs.sol
@@ -1,14 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-/**
- * @title YourContractName
- * @dev Licensed under Business Source License 1.1.
- *
- * You may not use this code in any production or competing service without
- * written permission from the licensor. The license changes to Apache 2.0
- * on January 1, 2028. See the LICENSE file for full details.
- */
-
 pragma solidity ^0.8.27;
 
 struct TickData {
