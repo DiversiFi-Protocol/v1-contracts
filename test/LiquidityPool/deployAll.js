43dbed4b630de53029ae04a570dbb3319a57135f
sblevins <stephenblevins@comcast.net>
2025-05-27 16:32:00 +1100

    added admin function tests

---
+++ test/LiquidityPool/deployAll.js
@@ -0,0 +1,108 @@
+const utils = require("../testModules/utils");
+const { getAddress, getCreateAddress } = require("ethers");
+
+module.exports = async function deployAll() {
+    const tokenName = "Diversified USD";
+    const tokenSymbol = "USD1";
+    const mintable0Decimals = 18n;
+    const mintable0TargetAllocation = utils.formatAllocationFromDecimal(0.333333333333333333333333333333333)
+    const mintable1Decimals = 20n;
+    const mintable1TargetAllocation = utils.formatAllocationFromDecimal(0.333333333333333333333333333333333)
+    const mintable2Decimals = 6n;
+    //the remaining allocation goes to mintable2
+    const mintable2TargetAllocation = (2n ** 88n - 1n) - mintable0TargetAllocation - mintable1TargetAllocation;
+    const [admin, unpriviledged] = await hre.ethers.getSigners();
+    const liquidityPoolAddress = getCreateAddress({
+      from: admin.address,
+      nonce: 1n,
+    });
+
+    const liquidityToken = await hre.ethers.deployContract("LiquidityToken", [
+      tokenName,
+      tokenSymbol,
+      liquidityPoolAddress,
+      admin.address,
+    ]);
+
+    const liquidityPool = await hre.ethers.deployContract("LiquidityPool", [
+      await admin.getAddress(),
+      await liquidityToken.getAddress(),
+    ]);
+
+    const mintable0 = await hre.ethers.deployContract("MintableERC20", [
+      "Mintable0",
+      "M0",
+      mintable0Decimals,
+    ]);
+    const mintable1 = await hre.ethers.deployContract("MintableERC20", [
+      "Mintable1",
+      "M1",
+      mintable1Decimals,
+    ]);
+    const mintable2 = await hre.ethers.deployContract("MintableERC20", [
+      "Mintable2",
+      "M2",
+      mintable2Decimals,
+    ]);
+
+    await mintable0.mint(admin.address, utils.MAX_UINT_256 / 2n)
+    await mintable1.mint(admin.address, utils.MAX_UINT_256 / 2n)
+    await mintable2.mint(admin.address, utils.MAX_UINT_256 / 2n)
+    await mintable0.approve(liquidityPool.target, utils.MAX_UINT_256)
+    await mintable1.approve(liquidityPool.target, utils.MAX_UINT_256)
+    await mintable2.approve(liquidityPool.target, utils.MAX_UINT_256)
+
+    await mintable0.mint(unpriviledged.address, utils.MAX_UINT_256 / 2n)
+    await mintable1.mint(unpriviledged.address, utils.MAX_UINT_256 / 2n)
+    await mintable2.mint(unpriviledged.address, utils.MAX_UINT_256 / 2n)
+    await mintable0.attach(unpriviledged).approve(liquidityPool.target, utils.MAX_UINT_256)
+    await mintable1.attach(unpriviledged).approve(liquidityPool.target, utils.MAX_UINT_256)
+    await mintable2.attach(unpriviledged).approve(liquidityPool.target, utils.MAX_UINT_256)
+
+    assetParams0 = {
+      decimals: mintable0Decimals,
+      targetAllocation: mintable0TargetAllocation,
+      assetAddress: getAddress(mintable0.target),
+    }
+    assetParams1 = {
+      decimals: mintable1Decimals,
+      targetAllocation: mintable1TargetAllocation,
+      assetAddress: getAddress(mintable1.target),
+    }
+    assetParams2 = {
+      decimals: mintable2Decimals,
+      targetAllocation: mintable2TargetAllocation,
+      assetAddress: getAddress(mintable2.target),
+    }
+    await liquidityPool.setAssetParams([
+      assetParams0,
+      assetParams1,
+      assetParams2,
+    ]);
+    const maxReserves = utils.MAX_UINT_256 / 2n
+    const maxReservesIncreaseRateQ128 = utils.decimalToFixed(0.1);
+    await liquidityPool.setMaxReserves(maxReserves);
+    const setMaxReservesBlock = await hre.ethers.provider.getBlock('latest');
+    const setMaxReservesTimestamp = setMaxReservesBlock.timestamp;
+    await liquidityPool.setMaxReservesIncreaseRateQ128(maxReservesIncreaseRateQ128);
+    await liquidityPool.setMintFeeQ128(utils.decimalToFixed(0.01)); // 1% mint fee
+    await liquidityPool.setBurnFeeQ128(utils.decimalToFixed(0.02)); // 2% burn fee
+
+    return {
+      liquidityToken,
+      liquidityPool,
+      admin,
+      unpriviledged,
+      tokenName,
+      tokenSymbol,
+      mintable0,
+      mintable1,
+      mintable2,
+      maxReserves,
+      maxReservesIncreaseRateQ128,
+      assetParams0,
+      assetParams1,
+      assetParams2,
+      setMaxReservesTimestamp
+    };
+  }
