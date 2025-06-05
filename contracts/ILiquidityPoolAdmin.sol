db51bee860ab3645e5f2c3e0df4fb624cabeeb58
sblevins <stephenblevins@comcast.net>
2025-06-05 15:37:49 +1100

    ergonomics edit

--- contracts/ILiquidityPoolAdmin.sol
+++ contracts/ILiquidityPoolAdmin.sol
@@ -16,14 +16,14 @@ import "./DataStructs.sol";
 /// @notice These are administrative functions not for regular users and as such are not documented.
 /// Anyone wishing to understand them will have to interpret the code.
 interface ILiquidityPoolAdmin {
-  function setAdmin(address _admin) external;
-  function setMintFeeQ128(uint256 _mintFeeQ128) external;
-  function setBurnFeeQ128(uint256 _burnFeeQ128) external;
-  function setMaxReserves(uint256 _maxReserves) external;
-  function setMaxReservesIncreaseRateQ128(uint256 _maxReservesIncreaseRateQ128) external;
-  function setMaxReservesIncreaseCooldown(uint256 _maxReservesIncreaseCooldown) external;
-  function setTargetAssetParams(AssetParams[] calldata _params) external;
-  function withdrawFees(address _recipient) external;
-  function setIsMintEnabled(bool _isMintEnabled) external;
-  function setEqualizationBounty(uint256 _equalizationBounty) external;
+  function setAdmin(address admin) external;
+  function setMintFeeQ128(uint256 mintFeeQ128) external;
+  function setBurnFeeQ128(uint256 burnFeeQ128) external;
+  function setMaxReserves(uint256 maxReserves) external;
+  function setMaxReservesIncreaseRateQ128(uint256 maxReservesIncreaseRateQ128) external;
+  function setMaxReservesIncreaseCooldown(uint256 maxReservesIncreaseCooldown) external;
+  function setTargetAssetParams(AssetParams[] calldata params) external;
+  function withdrawFees(address recipient) external;
+  function setIsMintEnabled(bool isMintEnabled) external;
+  function setEqualizationBounty(uint256 equalizationBounty) external;
 }
