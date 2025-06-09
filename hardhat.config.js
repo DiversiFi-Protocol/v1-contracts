230c89c194676fb97e91fb7acc6d16c9da168ea9
sblevins <stephenblevins@comcast.net>
2025-06-09 11:27:38 +1100

    I was forced against my will to upgrade hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,60 +1,59 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-gas-reporter")
+require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  sourcify: {
-    enabled: true
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY, // or put your key directly (not recommended)
-  },
-  solidity: {
-    version: "0.8.27",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 100000,
-      },
-    },
-  },
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-    gasPrice: 5,
-  },
-  networks: {
-    hardhat: {
-      gas: 6_000_000,
-    },
-    mainnet: {
-      url: "https://eth-mainnet.g.alchemy.com/v2/yHITndLemsVURB6z0335Y5aX3PHzRiXZ", // bravo labs deployer
-      chainId: 1,
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-        path: "m/420'/69'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: "",
-      },
-    },
-    sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/yHITndLemsVURB6z0335Y5aX3PHzRiXZ", // bravo labs deployer
-      chainId: 11155111,
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-        path: "m/420'/69'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: "",
-      },
-    },
-  },
-  contractSizer: {
-    runOnCompile: true,
-    strict: true,
-  },
+	sourcify: {
+		enabled: true,
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY, // or put your key directly (not recommended)
+	},
+	solidity: {
+		version: "0.8.27",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 100000,
+			},
+		},
+	},
+	gasReporter: {
+		enabled: true,
+		currency: "USD",
+		gasPrice: 5,
+	},
+	networks: {
+		hardhat: {
+			gas: 6_000_000,
+		},
+		mainnet: {
+			url: "https://eth-mainnet.g.alchemy.com/v2/yHITndLemsVURB6z0335Y5aX3PHzRiXZ", // bravo labs deployer
+			chainId: 1,
+			accounts: {
+				mnemonic: "test test test test test test test test test test test junk", //process.env.MNEMONIC,
+				// path: "m/420'/69'/0'/0",
+				// initialIndex: 0,
+				// count: 20,
+				// passphrase: "",
+			},
+		},
+		sepolia: {
+			url: "https://eth-sepolia.g.alchemy.com/v2/yHITndLemsVURB6z0335Y5aX3PHzRiXZ", // bravo labs deployer
+			chainId: 11155111,
+			accounts: {
+				mnemonic: "test test test test test test test test test test test junk", //process.env.MNEMONIC,
+				// path: "m/420'/69'/0'/0",
+				// initialIndex: 0,
+				// count: 20,
+				// passphrase: "",
+			},
+		},
+	},
+	contractSizer: {
+		runOnCompile: true,
+		strict: true,
+	},
 };
-
