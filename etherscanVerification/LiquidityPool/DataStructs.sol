d1efe1142b20c38a145927a96c933a09c945a032
sblevins <stephenblevins@comcast.net>
2025-06-15 11:17:44 +1100

    removed verification folder

--- etherscanVerification/LiquidityPool/DataStructs.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-
-pragma solidity ^0.8.27;
-
-struct AssetParams {
-  address assetAddress;
-  uint88 targetAllocation; // 0.88 fixed point
-  uint8 decimals;
-}
