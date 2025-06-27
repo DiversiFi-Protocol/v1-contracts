724ef38ef09eb02d9cef3897f98536a5719e118b
Stephen-BravoLabs <stephenblevins@bravolabs.xyz>
2025-06-27 14:57:01 +1100

    added flash mint and burn support

--- contracts/ILiquidityPoolCallback.sol
+++ contracts/ILiquidityPoolCallback.sol
@@ -7,6 +7,6 @@
 pragma solidity ^0.8.27;
 
 interface ILiquidityPoolCallback {
-  function mintCallback(bytes calldata forwardData) external;
-  function burnCallback(bytes calldata forwardData) external;
+  function dfiV1FlashMintCallback(bytes calldata forwardData) external;
+  function dfiV1FlashBurnCallback(bytes calldata forwardData) external;
 }
