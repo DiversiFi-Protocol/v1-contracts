ade0ce7ee3755d81eff0f065c0c0981733486029
sblevins <stephenblevins@comcast.net>
2025-07-22 08:04:19 +1100

    updated interface to include erc20 permit

--- contracts/IERC20MintAndBurn.sol
+++ contracts/IERC20MintAndBurn.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.27;
 
-import "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 
-interface IERC20MintAndBurn is IERC20 {
+interface IERC20MintAndBurn is IERC20Permit {
   function mint(address recipient, uint256 amount) external;
   function burnFrom(address burnAddress, uint256 amount) external;
   function decimals() external returns (uint8);
