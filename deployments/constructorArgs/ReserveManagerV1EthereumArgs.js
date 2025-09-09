fbf287f1a730f1ecd866c64dfa3de990a97b6006
Stephen-BravoLabs <stephenblevins@bravolabs.xyz>
2025-09-10 10:15:50 +1100

    removed old contract references

--- deployments/constructorArgs/ReserveManagerV1EthereumArgs.js
+++ deployments/constructorArgs/ReserveManagerV1EthereumArgs.js
@@ -3,7 +3,7 @@ const initialAssetParams = require("../initialAssetParams")
 module.exports = [
   "0x0F688325E86eDf0d0Da187f436a46C47C8F6CDC3",//TimelockController address
   "0x0F688325E86eDf0d0Da187f436a46C47C8F6CDC3",//multisig admin address
-  "0xDF10bE61122b11c89238445783b5b9DBfddE0EBf",//indexToken address
+  "",//indexToken address
   0,//mint fee
   0,//burn fee
   100000000000000000000000000n,//initial max reserves
