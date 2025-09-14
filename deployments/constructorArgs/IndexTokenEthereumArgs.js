01a8cf97e806c621dc0bcf447340f2d8d2e643d4
Stephen-BravoLabs <stephenblevins@bravolabs.xyz>
2025-09-15 09:51:46 +1100

    deploy args

--- deployments/constructorArgs/IndexTokenEthereumArgs.js
+++ deployments/constructorArgs/IndexTokenEthereumArgs.js
@@ -3,8 +3,9 @@ const utils = require("../../test/testModules/utils.js")
 module.exports = [
   "Diversified USD",
   "DFiUSD",
-  "0x0F688325E86eDf0d0Da187f436a46C47C8F6CDC3",//timelock controller
-  "",//reserve manager
+  "0x00890D5630f8e46aCFabDE50cD260acc5c605D33",//timelock controller
+  "0x8978F3A0227453Dd0821bC8A85e4bEF96DAF8327",//reserve manager
+  ["0xD5ade97228C6d11B25aDc8A50AFc2d73fEEa2D8D"],//mainainter
   43200,//12 hour minimum balance change delay
   utils.decimalToFixed(1.0000027639846123)//equivalent to 1.01 max change per hour
 ]
